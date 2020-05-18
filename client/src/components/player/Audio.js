8e1bef96ae23af169e72143df83cd52466e8ea1f
Parr <tedblount01@gmail.com>
2020-05-17 19:14:51 -0500

    I added some script to the audio component so the players would pause when you start another player

--- client/src/components/player/Audio.js
+++ client/src/components/player/Audio.js
@@ -27,6 +27,16 @@ console.log(files)
             <form method='post' action={`/files/${file._id}>)}?_method=DELETE`}>
                     <button>Delete</button>
                 </form>
+                <script>
+        {document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true)};
+</script>
             </div> : null
             )}
         </div>
