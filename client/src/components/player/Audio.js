b021d18e13b1b022502107f5fdaf9a54a4d32543
Andrew Bjork <bjorkandrew1@gmail.com>
2020-05-21 20:19:43 -0500

    nav

--- client/src/components/player/Audio.js
+++ client/src/components/player/Audio.js
@@ -31,7 +31,7 @@ console.log(files)
         {document.addEventListener('play', function(e){
     var audios = document.getElementsByTagName('audio');
     for(var i = 0, len = audios.length; i < len;i++){
-        if(audios[i] != e.target){
+        if(audios[i] !== e.target){
             audios[i].pause();
         }
     }
