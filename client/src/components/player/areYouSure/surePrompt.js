32713d133ed6a62550a1746df6a35f7ba5633d4b
Parr <tedblount01@gmail.com>
2020-05-26 21:04:44 -0500

    onclick table structure are you sure

--- client/src/components/player/areYouSure/surePrompt.js
+++ client/src/components/player/areYouSure/surePrompt.js
@@ -1,3 +1,9 @@
-export default function areYouSure() {
-    window.confirm("Are you sure you want to delete this?");
-};
+ export default function areYouSure(res) {
+    const sure = window.confirm("Are you sure?")
+    console.log(sure)
+    if (sure) {
+        console.log("yaay")
+        fetch(`/files/`+res, {method: 'DELETE'})
+        .then(() => {})
+    }
+    };
